4c67ee6439fa0c3931d43f01ca1b11dc85c86cb6
beqa <beqa.tskhvediani@gmail.com>
2024-02-21 10:10:55 +0400

    add container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import "./App.css";
 function App() {
   return (
     <section>
-      <h1>hello</h1>
-      <button>click</button>
-      <ul>
-        <li>1</li>
-      </ul>
+      <div className="container">
+        <div className="blue-container"></div>
+      </div>
     </section>
   );
 }
