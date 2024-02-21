7e0731af671f3ac8383eb6461fcb8f3acb4953ce
beqa <beqa.tskhvediani@gmail.com>
2024-02-21 10:40:10 +0400

    add blue-container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <section>
       <div className="container">
-        <div className="blue-container"></div>
+        <div className="blue-container">
+          <h1>Your Result</h1>
+          <div className="oval">
+            <span className="number">76</span>
+            <span className="of-100">of 100</span>
+          </div>
+          <h2 className="great">Great</h2>
+          <p className="paragraph">
+            Your performance exceed 65% of the people conducting the test here!
+          </p>
+        </div>
       </div>
     </section>
   );
