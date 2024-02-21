7e84b754742304d760775f011cd9c1d107063a70
beqa <beqa.tskhvediani@gmail.com>
2024-02-21 11:54:32 +0400

    add white container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./reset.css";
 import "./App.css";
 
+import logo1 from "./images/iconoir_flash.png";
+import logo2 from "./images/Group.png";
+import logo3 from "./images/chat.png";
+import logo4 from "./images/iconoir_flash.png";
+
 function App() {
   return (
     <section>
@@ -15,6 +20,48 @@ function App() {
           <p className="paragraph">
             Your performance exceed 65% of the people conducting the test here!
           </p>
+        </div>
+        <div className="white-container">
+          <h2 className="summery">Summery</h2>
+          <div className="wrapper">
+            <div className="rectangle">
+              <div className="rectangle-react">
+                <img src={logo1} aria-hidden alt="image-icone" />
+                <p className="react">Reaction</p>
+              </div>
+              <p className="rectangle-paragraph">
+                <span className="para-span">80</span>/100
+              </p>
+            </div>
+            <div className="rectangle-memory">
+              <div className="rectangle-react">
+                <img src={logo2} aria-hidden alt="image-icone" />
+                <p className="react-memory">Memory</p>
+              </div>
+              <p className="rectangle-paragraph">
+                <span className="para-span">92</span>/100
+              </p>
+            </div>
+            <div className="rectangle-verbal">
+              <div className="rectangle-react">
+                <img src={logo3} aria-hidden alt="image-icone" />
+                <p className="react-verbal">Verbal</p>
+              </div>
+              <p className="rectangle-paragraph">
+                <span className="para-span">61</span>/100
+              </p>
+            </div>
+            <div className="rectangle-visual">
+              <div className="rectangle-react">
+                <img src={logo4} aria-hidden alt="image-icone" />
+                <p className="react-visual">Visual</p>
+              </div>
+              <p className="rectangle-paragraph">
+                <span className="para-span">73</span>/100
+              </p>
+            </div>
+          </div>
+          <button>Continue</button>
         </div>
       </div>
     </section>
